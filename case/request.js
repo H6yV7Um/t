0f92daf953d308848fa14f69e43ba8ea29a9cbd0
kavinkhuang <kavinkhuang@tencent.com>
2018-05-23 18:25:20 +0800

    update

--- case/request.js
+++ case/request.js
@@ -3,29 +3,61 @@ const request = require('request');
 /**
  * config
  */
-const url = 'http://www.qq.com?debug=1'
+const url = 'http://www.qq.com'
 const opts = {
     method   : `GET`,
-    cookies  : `uin=o02060293288`,
-    useAgent : `Mozilla/5.0 (iPhone; CPU iPhone OS 7_1_2 like Mac OS X) AppleWebKit/537.51.2 (KHTML, like Gecko) Mobile/11D257 QQ/7.6.5.302 NetType/WIFI  Mem/28`,
+    cookies  : {
+      
+    },
     proxy : {
         host  : ``,
         point : 80
     }
 }
 
+/**
+ * sys config
+ */
+const USERAGENT = {
+    'normal' : `Mozilla/5.0 (iPhone; CPU iPhone OS 7_1_2 like Mac OS X) AppleWebKit/537.51.2 (KHTML, like Gecko) Mobile/11D257 QQ/7.6.5.302 NetType/WIFI  Mem/28`
+}
+
+
+function stringify(object,split) {
+    let arr = [];
+    for ( let i in object ) {
+        arr.push(`${i}=${object[i]}`);
+    }
+    return arr.join(split)
+}
+
+
+function getUseAgent(key) {
+    return key && USERAGENT[key] ? USERAGENT[key] : USERAGENT.normal
+}
+
+function formatParams(url,opts) {
+    return {
+        url : url,
+        method : opts.method,
+        headers : {
+            "cookie" : stringify(opts.cookies,';'),
+            "user-agent" : getUseAgent(opts.useAgent)
+        }
+    }
+}
+
 
 function run(url,opts) {
     return new Promise((resolve,reject) => {
-        request({
-            url : url,
-            method : opts.method,
-            headers : {
-                "cookie" : opts.cookies,
-                "user-agent" : opts.useAgent
+        let params = formatParams(url,opts);
+        request(params,(error,responese,body) => {
+            if ( error ) {
+                reject(error);
+            } else {
+                console.log(responese);
             }
-        },(error,responese,body) => {
-            console.log(error,body);
+            
         })
     });
 }
